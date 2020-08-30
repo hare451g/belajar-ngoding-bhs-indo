f90b706a721628ed40f9546ecb8c17e09839e651
hendra sadewa <hendra@stockbit.com>
2020-08-30 11:24:59 +0700

    add article to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,23 @@
-type propTypes = {};
+import fs from 'fs';
+import Article from '../app/containers/Article';
 
-function HomePage(props: propTypes) {
-  return <div>Homey!</div>;
+type propTypes = {
+  content: string;
+};
+
+export async function getStaticProps() {
+  const path = `${process.cwd()}/public/content`;
+  const content = fs.readFileSync(`${path}/main.md`, 'utf-8');
+
+  return {
+    props: {
+      content,
+    },
+  };
+}
+
+function HomePage({ content = '' }: propTypes) {
+  return <Article content={content} />;
 }
 
 export default HomePage;
